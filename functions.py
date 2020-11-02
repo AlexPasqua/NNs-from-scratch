7282b3d46dbacf54398261f7cae414793af02ca7
Gaetano Antonicchio <71822148+gaetanoantonicchio@users.noreply.github.com>
2020-11-02 10:34:26 +0100

    Apply suggestions from code review
    Co-authored-by: Alex Pasquali <33461106+AlexPasqua@users.noreply.github.com>

--- functions.py
+++ functions.py
@@ -1,16 +1,11 @@
 import numpy as np
 
 
-    """
-    Contains utility activation functions and their respective derivatives
-    """
 
-    def __init__(self, function):
-        self.function = function
 
     ########################## Sigmoid Activation Function ###############################
 
-    def sigmoid(x):
+def sigmoid(x):
         """
         Computes the sigmoid function of the inputs received by the unit;
         ||| sigm_fun(x)= 1/[1+exp(-x)] |||
@@ -18,9 +13,9 @@ import numpy as np
         :param x: net -> input's weighted sum
         :return: output of the unit
         """
-        return 1. / (1. + np.exp(-x))
+    return 1. / (1. + np.exp(-x))
 
-    def sigmoid_deriv(x):
+def sigmoid_deriv(x):
         """
         Computes the derivative of the sigmoid function;
         ||| sigm_fun(x)' = sigm_fun*(1-sigm_fun) |||
@@ -28,11 +23,11 @@ import numpy as np
         :param x: net -> input's weighted sum
         :return: derivative of the sigmoid function
         """
-        return np.diag(sigmoid(x) * (1 - sigmoid(x)))
+    return np.diag(sigmoid(x) * (1 - sigmoid(x)))
 
     ############################# SoftMax Activation Function ###########################
 
-    def softmax(self, x):
+def softmax(x):
         """
         Computes the softmax function of the input received by the unit:
         ||| softmax_fun(x) = exp(x - max(x)) |||
@@ -40,10 +35,9 @@ import numpy as np
         :param x: net -> input's weighted sum
         :return: output of the unit
         """
-        softmax_fun = np.exp(x - np.max(x))
-        return softmax_fun
+    return np.exp(x - np.max(x))
 
-    def softmax_deriv(self, x): # to be completed
+def softmax_deriv(x): #TODO: to be completed
         """
         Computes the derivative of the softmax function:
         ||| softmax_fun(x)' = to be completed **** |||
@@ -51,10 +45,10 @@ import numpy as np
         :param x: net -> input's weighted sum
         :return: derivative of the softmax function
         """
-        softmax_fun = np.exp(x - np.max(x))
+    softmax_fun = np.exp(x - np.max(x))
 
     ############################# ReLU Activation Function #################################
-    def relu(self,x):
+def relu(x):
         """
         Computes the ReLU function:
         ||| relu_fun(x) = max(0,x) |||
@@ -62,9 +56,9 @@ import numpy as np
         :param x: net -> input's weighted sum
         :return: output of the unit
         """
-        return np.maximum(0, x)
+    return np.maximum(0, x)
 
-    def relu_deriv(self,x):
+def relu_deriv(x):
         """
         Computes the derivative of the ReLU function:
         |||  relu_fun(x)' = 0  if x<0   |||
@@ -73,10 +67,10 @@ import numpy as np
         :param x: net-> input's weighted sum
         :return: derivative of the ReLU function
         """
-        if x < 0:
-            return 0
-        else:
-            return 1
+    if x < 0:
+        return 0
+    else:
+        return 1
 
 
 if __name__ == '__main__':
