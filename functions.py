8e77953e04ba47975be4222e67d1458b9282cc2c
AlexPasqua <alexpasquali98@gmail.com>
2020-11-06 18:24:49 +0100

    Check ReLU and its deriv: OK

--- functions.py
+++ functions.py
@@ -38,11 +38,14 @@ def relu_deriv(x):
     :param x: net-> input's weighted sum
     :return: derivative of the ReLU in x
     """
-    return 0 if x < 0 else 1
+    return 0 if x <= 0 else 1
 
 
 if __name__ == '__main__':
     # TODO: aggiungi test qui
     print(f"Sigmoid(1): {sigmoid(1)}")
     print(f"Derivative of sigmoid(1): {sigmoid_deriv(1)}")
-    print(f"Softmax(1): {softmax(1)}")
+    print(f"ReLU(1): {relu(1)}")
+    print(f"ReLU(-3): {relu(-3)}")
+    print(f"Derivative of ReLU(1): {relu_deriv(1)}")
+    print(f"Derivative of ReLU(-3): {relu_deriv(-3)}")
