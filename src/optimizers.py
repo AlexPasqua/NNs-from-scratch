24215fdf708e3eb50b1bac7f650026d440cda395
AlexPasqua <alexpasquali98@gmail.com>
2020-11-23 15:01:34 +0100

    Created (empty) class Optimizer

---
+++ src/optimizers.py
@@ -0,0 +1,10 @@
+""" In this scripts are defined the optimizers used in the project """
+
+
+class Optimizer():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    opt = Optimizer()
