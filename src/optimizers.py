bd4299632df93966fcf98df3e20f0df6748cd5bf
AlexPasqua <alexpasquali98@gmail.com>
2020-11-23 15:07:41 +0100

    "Optimizer" class made abstract

--- src/optimizers.py
+++ src/optimizers.py
@@ -1,10 +1,16 @@
 """ In this scripts are defined the optimizers used in the project """
 
+from abc import ABC, abstractmethod
 
-class Optimizer():
+
+class Optimizer(ABC):
+    """
+    Abstract class representing a generic optimizer
+    """
+    @abstractmethod
     def __init__(self):
         pass
 
 
 if __name__ == '__main__':
-    opt = Optimizer()
+    pass
