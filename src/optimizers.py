7fc78290053c640b8a906d6608d1a3ac025b7ed0
AlexPasqua <alexpasquali98@gmail.com>
2020-12-01 11:56:10 +0100

    Went on with backprop cycle

--- src/optimizers.py
+++ src/optimizers.py
@@ -38,18 +38,31 @@ class Optimizer(ABC):
             curr_act = curr_layer.get_activation()
             if i > 0:  # if there exist a previous layer
                 prev_layer = self.nn.layers[i-1]
+                # curr_inputs: inputs of the current layer's units (same for every unit in the current layer)
                 curr_inputs = [unit.get_out() for unit in prev_layer.get_units()]
+                # d_net: derivs of the weighted sum wrt the weights
                 d_net = curr_inputs
             else:
                 d_net = inp
+
+            # derivs of units' output wrt units' weighted sum
             d_out = [curr_act.deriv(curr_unit.out) for curr_unit in curr_layer.get_units()]
 
+            # local gradients of each unit of the current layer
             local_grads = [d_out[j] * [d_net_i for d_net_i in d_net] for j in range(len(d_out))]
             # equivalent to:
             # local_grads = []
             # local_grads.append([d_out[j] * d_net_i for j in range(len(d_out)) for d_net_i in d_net])
 
-            # TODO: compute upstream gradient
+            # TODO: complete this for more layers
+            # upstream gradient on units' output
+            if i == 0:
+                upstream_grad = d_err
+
+            # recompute upstream gradient wrt units' weights
+            upstream_grad = upstream_grad * [lg for lg in local_grads]
+
+
 
 
 class SGD(Optimizer, ABC):
@@ -69,5 +82,5 @@ optimizers = {
 }
 
 if __name__ == '__main__':
-    opt = optimizers['sgd'](Network(input_dim=3, units_per_layer=[3, 2], acts=['relu', 'relu']), 'squared')
-    opt.optimize(inp=[0, 0, 0], target=[1, 1])
+    opt = optimizers['sgd'](Network(input_dim=3, units_per_layer=[1], acts=['relu']), 'squared')
+    opt.optimize(inp=[0.1, 0.1, 0.1], target=[1])
