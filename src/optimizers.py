ab25ec8e702d6aed729ef0c69201d83d7471acda
AlexPasqua <alexpasquali98@gmail.com>
2020-12-09 10:52:28 +0100

    Progress in backprop, delta calculated

--- src/optimizers.py
+++ src/optimizers.py
@@ -55,36 +55,41 @@ class SGD(Optimizer, ABC):
         d_out = [output_act.deriv(u.net) for u in output_layer.units]
         dNet_dOut = [u.w[j] for u in output_layer.units for j in range(len(u.w))]
 
+        # delta_next is the delta of the output layer
+        # 'next' is because it will be continuously overwritten with the delta of the next layer
+        # it's normal mult because the vecs have the same dimension and they are 'numpy ndarray'
+        delta_next = dErr_dOut * d_out
+
         # scan all layers from the penultimate to the first
         for i in reversed(range(len(self.__nn.layers) - 1)):
             curr_layer = self.__nn.layers[i]
             next_layer = self.__nn.layers[i + 1]
-            curr_act = curr_layer.act
 
             # gradient of the error wrt the outputs of the CURRENT layer
             dErr_dOut_new = np.zeros([len(curr_layer.units)])
             for j in range(len(curr_layer.units)):
-                dErr_dOut_new[j] = 0.    # it's already 0, but let's be sure
+                dErr_dOut_new[j] = 0.  # it's already 0, but let's be sure
                 offset = len(curr_layer.units)  # offset to select the right unit from the next layer
                 for l in range(len(next_layer.units)):
-                    # print(dErr_dOut)
-                    # print(d_out)
-                    # print(dNet_dOut)
-                    # print(j, l)
-                    # print()
                     # dNet_dOut[offset * l + j]: weight (deriv of net wrt out) on the connection j --> l
                     dErr_dOut_new[j] += dErr_dOut[l] * d_out[l] * dNet_dOut[offset * l + j]
             dErr_dOut = dErr_dOut_new
-            break
 
+            # take new d_out and d_net wrt the current layer (no more the next)
+            curr_act = curr_layer.act
+            d_out = [curr_act.deriv(u.net) for u in curr_layer.units]
+            d_net = [u.w[k] for u in curr_layer.units for k in range(len(u.w))]
+
+            # computer delta for the current layer
+            delta = [np.dot(delta_next, [u.w[j] for u in next_layer.units]) for j in range(len(curr_layer.units))]
+            delta = [delta[j] * d_out[j] for j in range(len(curr_layer.units))]
+            break
 
             ########################################
             # TODO: riprendi da qui
             ########################################
 
-
-
-                # d_out = [curr_act.deriv(u.net) for u in curr_layer.units]
+            # d_out = [curr_act.deriv(u.net) for u in curr_layer.units]
 
             # # deriv of net wrt the prev layer's outputs
             # # nextLayer_dNet_dOut = [u.w[j] for u in next_layer.units for j in range(len(u.w))]
