8eac3bc04182793bd25d5a6975bea9921200cd59
AlexPasqua <alexpasquali98@gmail.com>
2020-12-11 20:36:36 +0100

    Fixes some issues in weights_inits.py

--- src/tests/test_weights_inits.py
+++ src/tests/test_weights_inits.py
@@ -11,13 +11,6 @@ class TestWeightsInits(unittest.TestCase):
 
     def test_random_init(self):
         self.assertRaises(TypeError, rand_init, hello='ciao')
-    #     self.assertEqual(self.rand_init.type, 'random')
-    #     self.assertEqual(len(self.rand_init.w), self.n_weights)
-    #     self.assertLessEqual(self.rand_init.b, self.max)
-    #     self.assertGreaterEqual(self.rand_init.b, self.min)
-    #     for w in self.rand_init.w:
-    #         self.assertLessEqual(w, self.max)
-    #         self.assertGreaterEqual(w, self.min)
 
 
 if __name__ == '__main__':
