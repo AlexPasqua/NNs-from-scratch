2a21be86000f19146bd398fe79f9840f6ed35b4f
AlexPasqua <alexpasquali98@gmail.com>
2020-12-11 19:42:34 +0100

    -

--- src/tests/test_network.py
+++ src/tests/test_network.py
@@ -13,8 +13,9 @@ class TestNetwork(unittest.TestCase):
         self.assertRaises(ValueError, Network, input_dim=3, units_per_layer=[3, 0], acts=['relu', 'relu'])
         self.assertRaises(ValueError, Network, input_dim=2, units_per_layer=[6, 2], acts=['relu', 'hello'])
         self.assertRaises(AttributeError, Network, input_dim=3, units_per_layer=[3, 2], acts=['relu', 'relu', 'relu'])
-        self.assertRaises(ValueError, Layer, [Unit(w=[0.5, 0.5, 0.5], b=1, act=act_funcs['sigmoid']),
-                                              Unit(w=[0.5, 0.5, 0.5], b=1, act=act_funcs['relu'])]
+        self.assertRaises(ValueError, Layer,
+                          [Unit(w=[0.5, 0.5, 0.5], b=1, act=act_funcs['sigmoid']),
+                           Unit(w=[0.5, 0.5, 0.5], b=1, act=act_funcs['relu'])]
                           )
 
     def test_forward(self):
