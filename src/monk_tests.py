2ba9ae44be82791ba2e17070a371da2314ef95fa
AlexPasqua <alexpasquali98@gmail.com>
2020-12-16 16:48:20 +0100

    Monk dataset imported as pandas dataframe

--- src/monk_tests.py
+++ src/monk_tests.py
@@ -1,7 +1,14 @@
 """ Script to do tests with the Monk dataset """
+import pandas as pd
 from network import Network
 
 if __name__ == '__main__':
+    # read the data
+    col_names = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
+    monk1_train = pd.read_csv("../datasets/monks/monks-1.train", sep=' ', names=col_names)
+    monk1_train.set_index('Id', inplace=True)
+    # print(monk1_train.describe().T)
+
     parameters = {
         'input_dim': 3,
         'units_per_layer': (3, 2),
@@ -10,4 +17,4 @@ if __name__ == '__main__':
         'weights_value': 0.1
     }
     model = Network(**parameters)
-    model.print_net()
+    # model.print_net()
