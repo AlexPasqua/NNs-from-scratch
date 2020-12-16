86906edccb8c9ab9e08ec235e0d9b46cedcb5b1a
AlexPasqua <alexpasquali98@gmail.com>
2020-12-16 16:53:41 +0100

    Dataframes transformed into numpy ndarray

--- src/monk_tests.py
+++ src/monk_tests.py
@@ -1,13 +1,20 @@
 """ Script to do tests with the Monk dataset """
 import pandas as pd
+import numpy as np
 from network import Network
 
+
 if __name__ == '__main__':
-    # read the data
+    # read the dataset
     col_names = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
     monk1_train = pd.read_csv("../datasets/monks/monks-1.train", sep=' ', names=col_names)
     monk1_train.set_index('Id', inplace=True)
+    labels = monk1_train.pop('class')
     # print(monk1_train.describe().T)
+
+    # transform the dataset from pandas dataframe to numpy ndarray
+    monk1_train = monk1_train.to_numpy()
+    labels = labels.to_numpy()
 
     parameters = {
         'input_dim': 3,
