Fixed signs of things here and there

src/network.py
@@ -219,9 +219,9 @@ class Layer:
         """
         dOut_dNet = np.array([self.__act.deriv(u.net) for u in self.__units])
         delta = upstream_delta * dOut_dNet
-        self.gradient_b = delta
+        self.gradient_b = -delta
         self.gradient_w = [
-            delta[j] * self.__inputs[i]
+            -delta[j] * self.__inputs[i]
             for j in range(len(delta))
             for i in range(len(self.__inputs))
         ]
@@ -367,7 +367,7 @@ class Network:
         # output_act = output_layer.act
         # dOut_dNet = np.array([output_act.deriv(u.net) for u in output_layer.units])
         # curr_delta = dErr_dOut * dOut_dNet
-        curr_delta = -dErr_dOut
+        curr_delta = dErr_dOut
         for layer in reversed(self.layers):
             curr_delta = layer.backward_pass(curr_delta)
 

src/optimizers.py
@@ -62,7 +62,7 @@ class SGD(Optimizer, ABC):
 
         # cycle through epochs
         for epoch in range(epochs):
-            print(epoch)
+            print(f"Epoch: {epoch + 1}")
             # TODO: shuffle dataset
 
             epoch_error = [0.] * len(net.layers[-1].units)
@@ -98,7 +98,6 @@ class SGD(Optimizer, ABC):
                     net.layers[i].weights += self.lrn_rate * grad_net.layers[i].weights
                     net.layers[i].biases += self.lrn_rate * grad_net.layers[i].biases
 
-            print(count, '\n')
             epoch_error = np.sum(epoch_error) / len(epoch_error)
             errors.append(epoch_error / float(len(train_set)))
 