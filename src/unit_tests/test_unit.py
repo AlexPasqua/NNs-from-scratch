4ef477d441077fc6f84ab35bdd8d00910af7fb5e
AlexPasqua <alexpasquali98@gmail.com>
2020-12-19 22:59:09 +0100

    -

--- src/unit_tests/test_unit.py
+++ src/unit_tests/test_unit.py
@@ -7,6 +7,7 @@ class TestUnit(unittest.TestCase):
     value = 0.2
     n_weights = 3
     unit = Unit(act='relu', init_type='uniform', value=value, n_weights=n_weights)
+
     def test_creation(self):
         self.assertEqual(self.value, self.unit.b)
         for weight in self.unit.w:
