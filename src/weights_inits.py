ce7603ae0564ebf1a79d83de9ea4387bf4a80e8c
AlexPasqua <alexpasquali98@gmail.com>
2020-12-19 22:50:54 +0100

    refactored weights_inits.py

--- src/weights_inits.py
+++ src/weights_inits.py
@@ -6,17 +6,19 @@ def weights_inits(**kwargs):
         raise AttributeError("'type' must be passed to function weights_inits")
     init_type = kwargs['type']
     inits = {
-        'uniform': uniform_init,
-        'random': rand_init
+        'uniform': _uniform_init,
+        'random': _rand_init
     }
     return inits[init_type](**kwargs)
 
 
-def uniform_init(n_weights, value, **kwargs):
+def _uniform_init(n_weights, value, **kwargs):
     return [value] * n_weights if n_weights > 1 else value
 
 
-def rand_init(lower_lim, upper_lim, n_weights, **kwargs):
+def _rand_init(n_weights, lower_lim=0., upper_lim=1., **kwargs):
+    if lower_lim >= upper_lim:
+        raise ValueError(f"lower_lim must be <= than upper_lim")
     if n_weights > 1:
         return np.random.uniform(lower_lim, upper_lim, n_weights)
     else:
