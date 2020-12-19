689fd3cddf46daac8e5ff05dad0a6837bad613e9
AlexPasqua <alexpasquali98@gmail.com>
2020-12-19 22:50:38 +0100

    refactored optimizers.py

--- src/optimizers.py
+++ src/optimizers.py
@@ -1,14 +1,11 @@
 """ In this scripts are defined the optimizers used in the project """
 import math
-import random
 from abc import ABC, abstractmethod
 import tqdm as tqdm
-from losses import losses
+from functions import losses, metrics
 from network import *
 import numpy as np
 import matplotlib.pyplot as plt
-from metrics import metrics
-from network import *
 
 
 class Optimizer(ABC):
@@ -17,15 +14,25 @@ class Optimizer(ABC):
     (check 'ABC' documentation for more info about abstract classes in Python)
 
     Attributes:
-        nn: Neural Network --> 'Network' object
-        loss: loss function --> 'Function' object
+        net: ('Network' object) Neural Network to which apply the algorithm
+        loss: ('DerivableFunction' object) loss function
+        metr: ('Function' object) accuracy function
+        lrn_rate: (float) learning rate
     """
 
     @abstractmethod
-    def __init__(self, loss, metr, lrn_rate=0.01):
+    def __init__(self, net, loss, metr, lrn_rate=0.01):
+        # makes sure lrn_rate is a value between 0 and 1
+        if lrn_rate <= 0 or lrn_rate > 1:
+            raise ValueError('lrn_rate should be a value between 0 and 1, Got:{}'.format(lrn_rate))
+        self.__net = net
         self.__loss = losses[loss]
-        self.__metr = metrics[metr]
+        self.__metric = metrics[metr]
         self.__lrn_rate = lrn_rate
+
+    @property
+    def net(self):
+        return self.__net
 
     @property
     def loss(self):
@@ -33,22 +40,22 @@ class Optimizer(ABC):
 
     @property
     def metr(self):
-        return self.__metr
+        return self.__metric
 
     @property
     def lrn_rate(self):
         return self.__lrn_rate
 
 
-class SGD(Optimizer, ABC):
-    """ Stochastic Gradient Descent """
+class GradientDescent(Optimizer, ABC):
+    """ Gradient Descent """
+    def __init__(self, net, loss, metr, lrn_rate=0.01):
+        super(GradientDescent, self).__init__(net, loss, metr, lrn_rate)
+        self.__type = 'gd'
 
-    def __init__(self, nn, loss, metr, lrn_rate=0.01):
-        self.__nn = nn
-        super(SGD, self).__init__(loss, metr, lrn_rate)
-        # makes sure lrn_rate is a value between 0 and 1
-        if lrn_rate <= 0 or lrn_rate > 1:
-            raise ValueError('lrn_rate should be a value between 0 and 1, Got:{}'.format(lrn_rate))
+    @property
+    def type(self):
+        return self.__type
 
     def optimize(self, train_set, targets, epochs, batch_size=1):
         """
@@ -64,8 +71,8 @@ class SGD(Optimizer, ABC):
             targets = targets[np.newaxis, :]
 
         errors = []
-        metrics = []
-        net = self.__nn
+        metric_values = []
+        net = self.net
 
         # cycle through epochs
         for epoch in tqdm.tqdm(range(epochs), desc="Iterating over epochs"):
@@ -82,9 +89,9 @@ class SGD(Optimizer, ABC):
             for batch_index in range(math.ceil(len(train_set) / batch_size)):
                 start = batch_index * batch_size
                 end = start + batch_size
-                train_batch = train_set[start : end]
-                targets_batch = targets[start : end]
-                grad_net = net.get_struct()
+                train_batch = train_set[start: end]
+                targets_batch = targets[start: end]
+                grad_net = net.get_empty_gradnet()
 
                 # cycle through patterns and targets within a batch
                 for pattern, target in zip(train_batch, targets_batch):
@@ -92,124 +99,37 @@ class SGD(Optimizer, ABC):
                     epoch_error[:] += self.loss.func(predicted=net_outputs, target=target)
                     epoch_metric[:] += self.metr.func(predicted=net_outputs, target=target)
                     dErr_dOut = self.loss.deriv(predicted=net_outputs, target=target)
-                    net.propagate_back(dErr_dOut)   # set the layers' gradients
+                    # set the layers' gradients and add them into grad_net
+                    # (emulate pass by reference of grad_net using return and reassign)
+                    grad_net = net.propagate_back(dErr_dOut, grad_net)
 
                     # add up layers' gradients
-                    for i in range(len(net.layers)):
-                        grad_net.layers[i].weights += net.layers[i].gradient_w
-                        grad_net.layers[i].biases += net.layers[i].gradient_b
+                    # for i in range(len(net.layers)):
+                    #     grad_net.layers[i].weights += net.layers[i].__gradient_w
+                    #     grad_net.layers[i].biases += net.layers[i].__gradient_b
 
                 # weights update
-                for i in range(len(net.layers)):
-                    grad_net.layers[i].weights = np.array(grad_net.layers[i].weights)
-                    grad_net.layers[i].biases = np.array(grad_net.layers[i].biases)
-                    grad_net.layers[i].weights /= float(batch_size)
-                    grad_net.layers[i].biases /= float(batch_size)
-                    net.layers[i].weights += self.lrn_rate * grad_net.layers[i].weights
-                    net.layers[i].biases += self.lrn_rate * grad_net.layers[i].biases
+                for layer_index in range(len(net.layers)):
+                    # grad_net.layers[i].weights = np.array(grad_net.layers[i].weights)
+                    # grad_net.layers[i].biases = np.array(grad_net.layers[i].biases)
+                    # grad_net.layers[i].weights /= float(batch_size)
+                    # grad_net.layers[i].biases /= float(batch_size)
+                    grad_net[layer_index]['weights'] /= float(batch_size)
+                    grad_net[layer_index]['biases'] /= float(batch_size)
+                    net.layers[layer_index].weights += self.lrn_rate * grad_net[layer_index]['weights']
+                    net.layers[layer_index].biases += self.lrn_rate * grad_net[layer_index]['biases']
 
             epoch_error = np.sum(epoch_error) / float(len(epoch_error))
             epoch_metric = np.sum(epoch_metric) / float(len(epoch_metric))
             errors.append(epoch_error / float(len(train_set)))
-            metrics.append(epoch_metric / float(len(train_set)))
+            print(epoch_metric)
+            print(epoch_metric / float(len(train_set)))
+            metric_values.append(epoch_metric / float(len(train_set)))
 
-        plt.plot(range(epochs), metrics)
-        plt.show()
-
-        # OLD BACKPROP --> keep it as reference
-        # losses = []
-        # for pattern, target in zip(train_set, targets):
-        #     output_layer = self.__nn.layers[-1]
-        #     output_act = output_layer.act
-        #     net_outputs = self.__nn.forward(inp=pattern)
-        #
-        #     err = self.loss.func(predicted=net_outputs, target=target)
-        #     losses.append(err / len(err))
-        #     print('Loss: ', np.sum(err) / len(err))
-        #
-        #     dErr_dOut = self.loss.deriv(predicted=net_outputs, target=target)
-        #     dOut_dNet = [output_act.deriv(u.net) for u in output_layer.units]
-        #     delta = -dErr_dOut * dOut_dNet
-        #     delta_next = delta
-        #
-        #     # retrieve the inputs of the output layer to compute the weights update for the output layer
-        #     out_layer_inputs = self.__nn.layers[-2].outputs if len(self.__nn.layers) > 1 else pattern
-        #     dErr_dBiases = -delta
-        #     dErr_dWeights = [
-        #         -delta[j] * out_layer_inputs[i]
-        #         for j in range(len(delta))
-        #         for i in range(len(out_layer_inputs))
-        #     ]
-        #
-        #     # variables used for weights and biases updates
-        #     # delta_weights: list of lists --> layers x weights_in_layer
-        #     # delta_biases: list of lists --> layers x biases_in_layer
-        #     # delta_weights = delta_biases = [[]] * len(self.__nn.layers)
-        #     delta_weights = [[]] * len(self.__nn.layers)
-        #     delta_biases = [[]] * len(self.__nn.layers)
-        #     delta_weights[-1] = [-dErr_dWeights[i] for i in range(len(dErr_dWeights))]
-        #     delta_biases[-1] = [-dErr_dBiases[j] for j in range(len(dErr_dBiases))]
-        #
-        #     # scan all layers from the penultimate to the first
-        #     for layer_index in reversed(range(len(self.__nn.layers) - 1)):
-        #         curr_layer = self.__nn.layers[layer_index]
-        #         next_layer = self.__nn.layers[layer_index + 1]
-        #         n_curr_units = len(curr_layer.units)  # number of units in the current layer
-        #         n_next_units = len(next_layer.units)  # number of units in the next layer
-        #         curr_act = curr_layer.act
-        #
-        #         dOut_dNet = [curr_act.deriv(u.net) for u in curr_layer.units]
-        #
-        #         delta = [np.dot(delta_next, [u.w[j] for u in next_layer.units]) for j in range(n_curr_units)]
-        #         delta = np.multiply(delta, dOut_dNet)
-        #         delta_next = delta
-        #         # equivalent to:
-        #         # delta = np.zeros([len(curr_layer.units)])
-        #         # for j in range(len(curr_layer.units)):
-        #         #     for l in range(len(next_layer.units)):
-        #         #         delta[j] += next_layer.units[l].w[j] * delta_next[l]
-        #         #     delta[j] *= dOut_dNet[j]
-        #
-        #         curr_layer_inputs = self.__nn.layers[layer_index - 1].outputs if layer_index > 0 else pattern
-        #         dErr_dBiases = -delta
-        #         dErr_dWeights = [
-        #             -delta[j] * curr_layer_inputs[i]
-        #             for j in range(len(delta))
-        #             for i in range(len(curr_layer_inputs))
-        #         ]
-        #         delta_weights[layer_index] = [-dErr_dWeights[i] for i in range(len(dErr_dWeights))]
-        #         delta_biases[layer_index] = [-dErr_dBiases[j] for j in range(len(dErr_dBiases))]
-        #
-        #     # update weights and biases
-        #     for layer_index in range(len(self.__nn.layers)):
-        #         curr_layer = self.__nn.layers[layer_index]
-        #         curr_layer.weights += self.lrn_rate * np.array(delta_weights[layer_index])
-        #         curr_layer.biases += self.lrn_rate * np.array(delta_biases[layer_index])
-        #
-        # plt.plot(losses)
+        plt.plot(range(epochs), metric_values)
         # plt.show()
 
 
 optimizers = {
-    'sgd': SGD
+    'gd': GradientDescent
 }
-
-if __name__ == '__main__':
-    opt = optimizers['sgd'](
-        Network(
-            input_dim=3,
-            units_per_layer=[3, 3, 3, 1],
-            acts=['sigmoid', 'sigmoid', 'sigmoid', 'sigmoid'],
-            weights_init='uniform',
-            weights_value=0.5
-        ),
-        loss='squared',
-        lrn_rate=1
-    )
-    n_patterns = 20
-    inputs = np.reshape([0.1, 0.1, 0.1] * n_patterns, newshape=(n_patterns, 3))
-    targets = np.reshape([1] * n_patterns, newshape=(n_patterns, 1))
-    opt.optimize(train_set=np.array(inputs),
-                 targets=np.array(targets),
-                 epochs=3,
-                 batch_size=1)
