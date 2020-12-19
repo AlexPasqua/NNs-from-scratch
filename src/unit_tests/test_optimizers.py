8f9bef76456ae660f81c2bad5d67fdb651c2c1e3
AlexPasqua <alexpasquali98@gmail.com>
2020-12-19 22:55:09 +0100

    Added test_optimizers.py

---
+++ src/unit_tests/test_optimizers.py
@@ -0,0 +1,25 @@
+import unittest
+import numpy as np
+from optimizers import optimizers
+from network import Network
+
+
+class TestOptimizers(unittest.TestCase):
+    gd = optimizers['gd'](
+        Network(input_dim=2, units_per_layer=(2, 1), acts=('relu', 'sigmoid')),
+        'squared',
+        'bin_class_acc',
+        lrn_rate=0.2)
+
+    def test_gd(self):
+        training_set = np.array([[1, 1], [1, 0], [0, 1], [0, 0]])
+        training_labels = np.array([[1], [1], [0], [0]])
+        self.gd.optimize(
+            train_set=training_set,
+            targets=training_labels,
+            epochs=2,
+            batch_size=1)
+
+
+if __name__ == '__main__':
+    unittest.main()
