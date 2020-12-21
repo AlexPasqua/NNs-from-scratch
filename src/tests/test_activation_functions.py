f68af366866f4d172d1dbea1c788c1ec916ec98d
Gaetano Antonicchio <71822148+gaetanoantonicchio@users.noreply.github.com>
2020-12-21 20:01:25 +0100

    unit test added for Leaky Relu in test_activation_functions.py

--- src/tests/test_activation_functions.py
+++ src/tests/test_activation_functions.py
@@ -10,6 +10,10 @@ class TestActivationFunctions(unittest.TestCase):
         self.assertAlmostEqual(act_funcs['tanh'].func(1.), 0.7615941559557649)
         self.assertEqual(act_funcs['threshold'].func(-1.8), 0.)
         self.assertEqual(act_funcs['threshold'].func(0.5), 1.)
+        self.assertEqual(act_funcs['leaky_relu'].func(1.), 1.)
+        self.assertAlmostEqual(act_funcs['leaky_relu'].func(-1.), -0.01)
+
+
 
 
 
@@ -19,13 +23,15 @@ class TestActivationFunctions(unittest.TestCase):
         self.assertEqual(act_funcs['relu'].deriv(-3.), 0.)
         self.assertAlmostEqual(act_funcs['tanh'].deriv(1.), 0.41997434161402614)
         self.assertEqual(act_funcs['threshold'].deriv(1.), 0.)
+        self.assertEqual(act_funcs['leaky_relu'].deriv(1.), 1.)
+        self.assertAlmostEqual(act_funcs['leaky_relu'].deriv(-1.), 0.01)
 
 
     def test_exceptions(self):
         # check many combination
         # it may be enough to test the function 'check_is_number', but this way we check also that
         # we call 'check_is_number' in every activation function and deriv
-        activation = ['sigmoid', 'relu', 'tanh', 'threshold']
+        activation = ['sigmoid', 'relu', 'tanh', 'threshold','leaky_relu']
         attribute_test = ['hello', [1, 2, 3]]
 
         for act in activation:
