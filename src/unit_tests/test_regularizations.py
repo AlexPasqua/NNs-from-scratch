c1d1a93c9a819044f24338a7318e9ceb5fe5972b
Gaetano Antonicchio <71822148+gaetanoantonicchio@users.noreply.github.com>
2020-12-26 23:33:35 +0100

    unittest added for derivative of l2 regularization in test_regularizations.py

--- src/unit_tests/test_regularizations.py
+++ src/unit_tests/test_regularizations.py
@@ -1,17 +1,21 @@
 import unittest
 from regularizations import regularization
+import numpy as np
 
 
 class TestRegularizations(unittest.TestCase):
-    w = [1, 0.2, -1]
+    w = np.array([[1, 0.2, -1], [1, 0, 0.5]])
+    array_test = np.array([[0.4, 0.08, -0.4], [0.4, 0., 0.2]])
 
     def test_regularizations(self):
-        self.assertAlmostEqual(regularization['l1'].func(self.w, lambd=0.2), 0.44000)
-        self.assertAlmostEqual(regularization['l2'].func(self.w, lambd=0.2), 0.40800)
+        self.assertAlmostEqual(regularization['l1'].func(self.w, lambd=0.2), 0.7400000000000001)
+        self.assertAlmostEqual(regularization['l2'].func(self.w, lambd=0.2), 0.658)
+        np.testing.assert_array_almost_equal(regularization['l2'].deriv(self.w, lambd=0.2), self.array_test)
 
-    def test_ValueError(self):
-        # self.assertRaises(KeyError, regularization['nonexistent_reg'].func, self.w, lambd=0.1)
-        self.assertRaises(ValueError, regularization['l1'].func, self.w, lambd=-0.5)
+
+# def test_ValueError(self):
+# self.assertRaises(KeyError, regularization['nonexistent_reg'].func, self.w, lambd=0.1)
+# self.assertRaises(ValueError, regularization['l1'].func, self.w, lambd=-0.5)
 
 
 if __name__ == '__main__':
