1b97bc2efa14fdf127c1f15737335beda0ce8472
AlexPasqua <alexpasquali98@gmail.com>
2021-01-02 23:11:34 +0100

    Adapted test_function.py

--- src/unit_tests/test_function.py
+++ src/unit_tests/test_function.py
@@ -21,55 +21,55 @@ class TestActivationFunctions(unittest.TestCase):
     #     # it may be enough to test the function 'check_is_number', but this way we check also that
     #     # we call 'check_is_number' in every activation function and deriv
     #     activation = ['sigmoid', 'relu', 'leaky_relu', 'tanh']
-    #     attribute_test = [[1, [1, 2, 3]], 'hello']
+    #     attribute_test = [[1], 'hello']
     #     for act in activation:
     #         for attr_test in attribute_test:
-    #             self.assertRaises(AttributeError, act_funcs[act].func, attr_test)
-    #             self.assertRaises(AttributeError, act_funcs[act].deriv, attr_test)
+    #             self.assertRaises(TypeError, act_funcs[act].func, attr_test)
+    #             self.assertRaises(TypeError, act_funcs[act].deriv, attr_test)
 
 
-# class TestLossFunctions(unittest.TestCase):
-#     predicted = [1, 0, 0, 1]
-#     target = [1, 1, 0, 0]
-#
-#     def test_loss_funcs(self):
-#         ground_truth = [0., 0.5, 0., 0.5]
-#         for i in range(len(ground_truth)):
-#             self.assertEqual(losses['squared'].func(self.predicted, self.target)[i], ground_truth[i])
-#
-#     def test_loss_funcs_derivs(self):
-#         ground_truth = [0., -1., 0., 1.]
-#         for i in range(len(ground_truth)):
-#             self.assertEqual(losses['squared'].deriv(self.predicted, self.target)[i], ground_truth[i])
-#
-#     def test_exceptions(self):
-#         # test Exception raising with different shapes arrays
-#         self.assertRaises(Exception, losses['squared'].func, [0, 0], [0, 0, 0])
-#         self.assertRaises(Exception, losses['squared'].deriv, [0, 0], [0, 0, 0])
-#
-#
-# class TestMetrics(unittest.TestCase):
-#     def test_metrics(self):
-#         self.assertEqual(1, metrics['bin_class_acc'].func(predicted=[1], target=[0.98]))
-#         self.assertEqual(0, metrics['bin_class_acc'].func(predicted=[1], target=[0.098]))
-#
-#
-# class TestLRDecays(unittest.TestCase):
-#     def test_lr_decays(self):
-#         curr_lr = 0.5
-#         base_lr = 0.5
-#         final_lr = 0.05
-#         curr_step = 10
-#         limit_step = 100
-#         self.assertEqual(
-#             lr_decays['linear'].func(curr_lr=curr_lr, base_lr=base_lr, final_lr=final_lr, curr_step=curr_step, limit_step=limit_step),
-#             (1 - curr_step / limit_step) * base_lr + curr_step / limit_step * final_lr
-#         )
-#         curr_step = limit_step + 1
-#         self.assertEqual(
-#             lr_decays['linear'].func(curr_lr=curr_lr, base_lr=base_lr, final_lr=final_lr, curr_step=curr_step, limit_step=limit_step),
-#             final_lr
-#         )
+class TestLossFunctions(unittest.TestCase):
+    predicted = [1, 0, 0, 1]
+    target = [1, 1, 0, 0]
+
+    def test_loss_funcs(self):
+        ground_truth = [0., 0.5, 0., 0.5]
+        for i in range(len(ground_truth)):
+            self.assertEqual(losses['squared'].func(self.predicted, self.target)[i], ground_truth[i])
+
+    def test_loss_funcs_derivs(self):
+        ground_truth = [0., -1., 0., 1.]
+        for i in range(len(ground_truth)):
+            self.assertEqual(losses['squared'].deriv(self.predicted, self.target)[i], ground_truth[i])
+
+    def test_exceptions(self):
+        # test Exception raising with different shapes arrays
+        self.assertRaises(Exception, losses['squared'].func, [0, 0], [0, 0, 0])
+        self.assertRaises(Exception, losses['squared'].deriv, [0, 0], [0, 0, 0])
+
+
+class TestMetrics(unittest.TestCase):
+    def test_metrics(self):
+        self.assertEqual(1, metrics['bin_class_acc'].func(predicted=[1], target=[0.98]))
+        self.assertEqual(0, metrics['bin_class_acc'].func(predicted=[1], target=[0.098]))
+
+
+class TestLRDecays(unittest.TestCase):
+    def test_lr_decays(self):
+        curr_lr = 0.5
+        base_lr = 0.5
+        final_lr = 0.05
+        curr_step = 10
+        limit_step = 100
+        self.assertEqual(
+            lr_decays['linear'].func(curr_lr=curr_lr, base_lr=base_lr, final_lr=final_lr, curr_step=curr_step, limit_step=limit_step),
+            (1 - curr_step / limit_step) * base_lr + curr_step / limit_step * final_lr
+        )
+        curr_step = limit_step + 1
+        self.assertEqual(
+            lr_decays['linear'].func(curr_lr=curr_lr, base_lr=base_lr, final_lr=final_lr, curr_step=curr_step, limit_step=limit_step),
+            final_lr
+        )
 
 
 if __name__ == '__main__':
