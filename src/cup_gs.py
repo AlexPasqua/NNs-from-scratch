4c2234669a1a3b74969e58df231b6a5033203bf7
AlexPasqua <alexpasquali98@gmail.com>
2021-01-24 19:21:17 +0100

    Fixed cup_gs.py
    Co-authored-by: gaetanoantonicchio

--- src/cup_gs.py
+++ src/cup_gs.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     # read dataset
     devset_x, devset_y, int_ts_x, int_ts_y, ts_data = read_cup(int_ts=True)
 
-    # grid search
+    # grid search parameters
     gs_params = {'units_per_layer': ((20, 2), (20, 20, 2), (20, 20, 10, 2), (8, 8, 8, 8, 8, 2)),
                  'acts': (('leaky_relu', 'identity'), ('tanh', 'identity'),
                           ('leaky_relu', 'leaky_relu', 'identity'), ('tanh', 'tanh', 'identity'),
@@ -14,43 +14,26 @@ if __name__ == '__main__':
                           ('leaky_relu', 'leaky_relu', 'leaky_relu', 'leaky_relu', 'leaky_relu', 'identity'),
                           ('tanh', 'tanh', 'tanh', 'tanh', 'tanh', 'identity'),),
                  'init_type': ('uniform',),
-                 'limits': ((-0.001, 0.001),),
+                 'limits': ((-0.1, 0.1), (-0.001, 0.001)),
                  'momentum': (0.5, 0.8),
-                 'batch_size': ('full', 200, 1),
+                 'batch_size': (1, 200, 'full'),
                  'lr': (0.001, 0.0001),
                  'lr_decay': (None, 'linear', 'exponential'),
                  'limit_step': (400,),
                  'decay_rate': (0.95,),
                  'decay_steps': (400,),
                  'lambd': (0, 0.001, 0.0001, 0.00001),
-                 'reg_type': ('l2', 'l2'),
+                 'reg_type': ('l1', 'l2'),
                  'staircase': (True, False),
                  'loss': ('squared',),
                  'metr': ('euclidean',),
                  'epochs': (150, 400, 700)}
+
+    # coarse to fine grid search. Results are saved on file
     grid_search(dataset="cup", params=gs_params, coarse=True)
-    # _, best_params = get_best_models(dataset="cup", coarse=True, n_models=5)
-    # best_params = best_params[0]
-    # grid_search(data=devset_x, targets=devset_y, ds_name="cup", params=best_params, coarse=False, n_config=4)
-
-    # best_models, best_params = get_best_models(dataset="cup", coarse=False, n_models=10)
-    # for p in best_params:
-    #     print(p)
-
-    # best_model, best_params = best_models[0], best_params[0]
-    # best_params['epochs'] = 400
-    # best_model.compile(opt='sgd', **best_params)
-    # tr_error_values, tr_metric_values, val_error_values, val_metric_values = best_model.fit(
-    #     tr_x=devset_x, tr_y=devset_y, disable_tqdm=False, **best_params)
-    #
-    # # plot graph
-    # plot_curves(
-    #     tr_loss=tr_error_values,
-    #     val_loss=val_error_values,
-    #     tr_acc=tr_metric_values,
-    #     val_acc=val_metric_values,
-    #     path="gae_cloud.png"
-    # )
-    #
-    # res = best_model.evaluate(inp=int_ts_x, targets=int_ts_y, metr='euclidean', loss='squared', disable_tqdm=False)
-    # print(f"Err: {res[0]}\tMetr: {res[1]}")
+    _, best_params = get_best_models(dataset="cup", coarse=True, n_models=5)
+    best_params = best_params[0]
+    grid_search(dataset="cup", params=best_params, coarse=False, n_config=4)
+    best_models, best_params = get_best_models(dataset="cup", coarse=False, n_models=10)
+    for p in best_params:
+        print(p)
